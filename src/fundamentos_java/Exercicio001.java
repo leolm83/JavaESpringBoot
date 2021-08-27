08589c1c7a6526b338306945f461d555ad35c589
Leonardo Mateus <leonardo.mateus01@hotmail.com>
2021-08-27 16:38:05 -0300

    Voltando ao Java

---
+++ src/fundamentos_java/Exercicio001.java
@@ -0,0 +1,12 @@
+package fundamentos_java;
+
+public class Exercicio001 {
+
+	public static void main(String[] args) {
+
+		System.out.print("Olá Mundo !!!");
+		System.out.println("syso + ctrl+ espaco = System.out.println();");
+		System.out.println(" CTRL+SHIFT+F indenta o codigo!!");
+	}
+
+}
---
+++ src/fundamentos_java/Exercicio002.java
@@ -0,0 +1,26 @@
+package fundamentos_java;
+/**
+* <h1>Dicas Basicas</h1>
+*
+* <p>dicas basicas da linguagem Java usando a IDE Eclipse
+* </p>
+* 
+* @author  Leonardo Marques 
+* @version 1.0
+* @since   2021-08-27
+*@see <a>https://www.tutorialspoint.com/java/java_documentation.htm></a>
+*/
+public class Exercicio002 {
+			public static void main(String[] args) {
+				System.out.println("Para criar o método main é possivel pelo atalho digitando: main e ctrl+espaco");
+				System.out.println("CTRL + M  maximiza a tela no eclipse");
+				System.out.println("CTRL+/ comenta uma linha\n"
+						+ "Pesquisar sobre JavaDocs e o comentário de documentacao usando /** ");
+				/* isto é um comentário 
+				 de multiplas linhas */
+				//este é um comentario de unica linha
+				/**
+				 * este é um comentario de documentacao e normalmente nao fica no meio do programa
+				 */
+			}
+}
